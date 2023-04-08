9c3cb79f5e0d950173e03dbd8ca34bda6b874762
behilalemu <91044960+Behil-Alemu@users.noreply.github.com>
2023-04-07 21:54:11 -0400

    edited server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 'use strict';
 // require('dotenv').config();
 
-import app from './app';
+import app from './app.js';
 //port is listed as 3001 in config
 import { PORT } from './config';
 // listen at port 3001
