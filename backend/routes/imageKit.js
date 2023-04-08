0f90cd5b277d1b187ac350a915346ecd6b7c9e46
behilalemu <91044960+Behil-Alemu@users.noreply.github.com>
2023-04-07 23:26:09 -0400

    key added in code imageKit

--- backend/routes/imageKit.js
+++ backend/routes/imageKit.js
@@ -9,14 +9,14 @@ const ImageKit = require('imagekit');
 const router = express.Router();
 const axios = require('axios');
 
-
 //server for ImageKit backend authentication
 const imagekit = new ImageKit({
-	publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
-	privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+	publicKey: 'public_p8VaPHs7pS8AtsSZr/cs3QquWc8=',
+	privateKey: 'private_abPVKveUDzdPFpKpaVw6n6YFPA8=',
 	urlEndpoint: 'https://ik.imagekit.io/kwjg3hkrf'
 });
 
+console.log(process.env.IMAGEKIT_PUBLIC_KEY, process.env.IMAGEKIT_PRIVATE_KEY);
 // allow cross-origin requests
 app.use(function(req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
