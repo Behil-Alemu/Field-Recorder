4c3c053eb19695d1acb88df699cd3765636672d7
behilalemu <91044960+Behil-Alemu@users.noreply.github.com>
2023-04-07 21:47:25 -0400

    ES import used in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 'use strict';
 require('dotenv').config();
 
-const app = require('./app').default;
+import app from './app';
 //port is listed as 3001 in config
-const { PORT } = require('./config');
+import { PORT } from './config';
 // listen at port 3001
 app.listen(PORT, function() {
 	console.log(`Started on http://localhost:${PORT}`);
