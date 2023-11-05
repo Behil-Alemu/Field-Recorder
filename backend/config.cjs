3e13e2c7b10b83aaa46838e1f8228ec54081ef63
behilalemu <91044960+Behil-Alemu@users.noreply.github.com>
2023-11-05 16:41:56 -0500

    local only

--- backend/config.cjs
+++ backend/config.cjs
@@ -13,7 +13,7 @@ const PORT = +process.env.PORT || 3001;
 function getDatabaseUri() {
   return (process.env.NODE_ENV === "test")
       ? "sample_col_test"
-      : process.env.DATABASE_URL || "sample_col";
+      : "sample_col";
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
